d4be0f33f62481fdc545258c3c521ad27cc4b3cf
timibadass <omoyenitimi@gmail.com>
2020-06-05 22:03:22 +0100

    add code for using transitions with one/two element

---
+++ vue-transitions/src/components/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <transition name="fade">
+      <p v-if="show" key="visible">Now you see me!</p>
+      <p v-else key="notVisible">Now you don't!</p>
+    </transition>
+    <button @click="transitionMe">click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+ data() {
+   return {
+     show: true
+   }
+ },
+ methods: {
+   transitionMe() {
+     this.show = !this.show
+   }
+ }
+}
+</script>
+
+<style>
+p {
+  color: green;
+}
+.fade-enter-active {
+  transition: transform .3s cubic-bezier(1.0, 0.5, 0.8, 1.0), color .5s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.fade-leave-active {
+  transition: transform 1s cubic-bezier(1.0, 0.5, 0.8, 1.0), color 1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+
+.fade-enter{
+  color: mediumblue;
+  transform: translateY(20px);
+}
+
+.fade-leave-to {
+  transform: translateX(100px);
+  color: cyan;
+}
+
+</style>
